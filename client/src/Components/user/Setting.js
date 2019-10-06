796b77551d339c73293990a1385b97f3f49db1b3
Administrator <admin@example.com>
2019-10-06 22:49:07 +0100

    add Private Route, setting page

---
+++ client/src/Components/user/Setting.js
@@ -0,0 +1,188 @@
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Navbar from "../layouts/Navbar";
+import Footer from "../layouts/Footer";
+
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: "#e74c3c"
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: "#e74c3c",
+    "&:hover": {
+      backgroundColor: "transparent",
+      color: "#e74c3c",
+      border: "1px solid #e74c3c"
+    }
+  }
+}));
+
+const Form = () => {
+  const classes = useStyles();
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    oldPassword: "",
+    newPassword: "",
+    newPassword2: ""
+  });
+
+  const {
+    firstName,
+    lastName,
+    username,
+    email,
+    oldPassword,
+    newPassword,
+    newPassword2
+  } = formData;
+
+  const handleChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  return (
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <AccountCircle />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Update Account Informations
+        </Typography>
+        <form className={classes.form}>
+          <TextField
+            variant='standard'
+            margin='normal'
+            fullWidth
+            id='first-name'
+            label='First Name'
+            name='firstName'
+            value={firstName}
+            onChange={e => handleChange(e)}
+            autoFocus
+          />
+
+          <TextField
+            variant='standard'
+            margin='normal'
+            fullWidth
+            id='last-name'
+            label='Last Name'
+            name='lastName'
+            value={lastName}
+            onChange={e => handleChange(e)}
+          />
+
+          <TextField
+            variant='standard'
+            margin='normal'
+            fullWidth
+            id='username'
+            label='Username'
+            name='username'
+            autoComplete='username'
+            value={username}
+            onChange={e => handleChange(e)}
+          />
+
+          <TextField
+            variant='standard'
+            margin='normal'
+            fullWidth
+            id='email'
+            label='Email'
+            name='email'
+            autoComplete='email'
+            value={email}
+            onChange={e => handleChange(e)}
+          />
+
+          <TextField
+            variant='standard'
+            margin='normal'
+            fullWidth
+            name='oldPassword'
+            label='Old Password'
+            type='password'
+            id='oldpassword'
+            value={oldPassword}
+            onChange={e => handleChange(e)}
+          />
+
+          <TextField
+            variant='standard'
+            margin='normal'
+            fullWidth
+            name='newPassword'
+            label='New Password'
+            type='password'
+            id='newpassword'
+            value={newPassword}
+            onChange={e => handleChange(e)}
+          />
+          <TextField
+            variant='standard'
+            margin='normal'
+            fullWidth
+            name='newPassword2'
+            label='Confirm New Password'
+            type='password'
+            id='newpassword2'
+            value={newPassword2}
+            onChange={e => handleChange(e)}
+          />
+
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+          >
+            Update
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+const Setting = () => {
+  return (
+    <>
+      <Navbar />
+      <Form />
+      <Footer />
+    </>
+  );
+};
+
+export default Setting;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./Components/PrivateRoute";
 import Register from "./Components/auth/Register";
 import Login from "./Components/auth/Login";
 import Recovery from "./Components/auth/Recovery";
+import Setting from "./Components/user/Setting";
 import Landing from "./Components/layouts/Landing";
 import "./index.css";
 
@@ -16,6 +18,7 @@ function App() {
           <Route exact path='/login' component={Login} />
           <Route exact path='/recovery' component={Recovery} />
           <Route exact path='/' component={Landing} />
+          <PrivateRoute to='/setting' component={Setting} isLogged={true} />
         </Switch>
       </BrowserRouter>
     </>
