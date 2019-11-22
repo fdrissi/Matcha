a5a59e71ed3d470d05eab7b607452febb5a59ff7
Administrator <admin@example.com>
2019-11-22 16:50:55 +0100

    setting

--- client/src/Components/user/Setting.js
+++ client/src/Components/user/Setting.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Redirect } from "react-router-dom";
 import { useUserStore } from "../../Context/appStore";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
