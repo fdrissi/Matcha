0b378fccb0910a8f0a211b8ddc1687cedd4b1997
Administrator <admin@example.com>
2019-12-09 16:07:19 +0100

    add trycatch block to users api

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,30 +13,34 @@ const middleware = require("../../middleware/midlleware");
 // @desc    Login User
 // @access  Public
 router.post("/login", async (req, res) => {
-  const { email, password, remember } = req.body;
-  const user = await userModel.login(email);
-  if (!user) {
-    return res.json({ success: false, errorMsg: "Wrong Credentials" });
-  }
-  const passwordMtach = await bcrypt.compare(password, user.password);
-  if (!passwordMtach)
-    return res.json({ success: false, errorMsg: "Wrong Credentials" });
-  if (!user.verified)
-    return res.json({ success: false, errorMsg: "Verify your account" });
-
-  const payload = {
-    user: {
-      id: user.id
+  try {
+    const { email, password, remember } = req.body;
+    const user = await userModel.login(email);
+    if (!user) {
+      return res.json({ success: false, errorMsg: "Wrong Credentials" });
     }
-  };
+    const passwordMtach = await bcrypt.compare(password, user.password);
+    if (!passwordMtach)
+      return res.json({ success: false, errorMsg: "Wrong Credentials" });
+    if (!user.verified)
+      return res.json({ success: false, errorMsg: "Verify your account" });
 
-  const expiration = remember ? 3600 * 24 * 14 : 3600;
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
 
-  jwt.sign(payload, key, { expiresIn: expiration }, (err, token) => {
-    if (err) throw err;
-    res.cookie("token", token, { httpOnly: true, maxAge: expiration * 1000 }); //one hour or 2 weeks
-    res.json({ success: true });
-  });
+    const expiration = remember ? 3600 * 24 * 14 : 3600;
+
+    jwt.sign(payload, key, { expiresIn: expiration }, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token, { httpOnly: true, maxAge: expiration * 1000 }); //one hour or 2 weeks
+      res.json({ success: true });
+    });
+  } catch (error) {
+    return res.json({ success: false, errorMsg: "Error Occured" });
+  }
 });
 
 // @route   POST api/users/logout
@@ -47,7 +51,7 @@ router.get("/logout", [middleware.auth], async (req, res) => {
     res.clearCookie("token");
     return res.json({ success: true });
   } catch (error) {
-    return res.json({ success: false, errorMsg: "Error" });
+    return res.json({ success: false, errorMsg: "Error Occured" });
   }
 });
 
@@ -55,28 +59,32 @@ router.get("/logout", [middleware.auth], async (req, res) => {
 // @desc    Register User
 // @access  Public
 router.post("/register", [middleware.register], async (req, res) => {
-  const errors = {
-    email: "",
-    userName: "",
-    firstName: "",
-    lastName: "",
-    password: "",
-    confirmPassword: ""
-  };
-  const user = await userModel.register(req.body);
-  if (!user) {
-    return res.json({
-      success: false,
-      errorMsg: "Sorry there is a problem with the database",
-      errors
-    });
-  } else {
-    res.json({
-      success: true,
-      SuccessMsg: `We'll send an email to ${req.body.email} In 5 minutes. Open it up to activate your account.`,
-      errorMsg: "Register Success",
-      errors
-    });
+  try {
+    const errors = {
+      email: "",
+      userName: "",
+      firstName: "",
+      lastName: "",
+      password: "",
+      confirmPassword: ""
+    };
+    const user = await userModel.register(req.body);
+    if (!user) {
+      return res.json({
+        success: false,
+        errorMsg: "Sorry there is a problem with the database",
+        errors
+      });
+    } else {
+      res.json({
+        success: true,
+        SuccessMsg: `We'll send an email to ${req.body.email} In 5 minutes. Open it up to activate your account.`,
+        errorMsg: "Register Success",
+        errors
+      });
+    }
+  } catch (error) {
+    return res.json({ success: false, errorMsg: "Error Occured" });
   }
 });
 
@@ -84,45 +92,16 @@ router.post("/register", [middleware.register], async (req, res) => {
 // @desc Recover User
 // @access PUBLIC
 router.post("/recover", async (req, res) => {
-  const { data } = req.body;
-  // first of all we have to check if the input is valide
-  const result = await userModel.findByUsername(data);
-  const respond = await userModel.checkByEamilUsernameValidation(data);
-  if (result) {
-    if (respond) {
-      const token = crypto.randomBytes(64).toString("hex");
-      // here we goona seet the token to that user (result = all row of the user get it from the users model)
-      const resultFromSr = userModel.setRecovery(result.email, token);
-      if (resultFromSr) {
-        // here we gonna send the email of the recovery
-        if (sendRecovery(result, token)) {
-          return res.json({
-            success: true,
-            errorMsg:
-              "A message has been sent to you by email with instructions on how to reset your password."
-          });
-        }
-      } else {
-        // something goes worng
-        return res.json({
-          success: false,
-          errorMsg: "Something goes wrong please lets know"
-        });
-      }
-    } else {
-      // the user need to validate his account firest
-      return res.json({
-        success: false,
-        errorMsg: "Please confirme your account first"
-      });
-    }
-  } else {
-    const result = await userModel.findByEmail(data);
+  try {
+    const { data } = req.body;
+    // first of all we have to check if the input is valide
+    const result = await userModel.findByUsername(data);
+    const respond = await userModel.checkByEamilUsernameValidation(data);
     if (result) {
       if (respond) {
-        // let send the user an email to recover his account
         const token = crypto.randomBytes(64).toString("hex");
-        const resultFromSr = userModel.setRecovery(data, token);
+        // here we goona seet the token to that user (result = all row of the user get it from the users model)
+        const resultFromSr = userModel.setRecovery(result.email, token);
         if (resultFromSr) {
           // here we gonna send the email of the recovery
           if (sendRecovery(result, token)) {
@@ -140,70 +119,116 @@ router.post("/recover", async (req, res) => {
           });
         }
       } else {
+        // the user need to validate his account firest
         return res.json({
           success: false,
           errorMsg: "Please confirme your account first"
         });
       }
     } else {
-      return res.json({
-        success: false,
-        errorMsg: "Invalid email or username. Please try to resubmit the form."
-      });
+      const result = await userModel.findByEmail(data);
+      if (result) {
+        if (respond) {
+          // let send the user an email to recover his account
+          const token = crypto.randomBytes(64).toString("hex");
+          const resultFromSr = userModel.setRecovery(data, token);
+          if (resultFromSr) {
+            // here we gonna send the email of the recovery
+            if (sendRecovery(result, token)) {
+              return res.json({
+                success: true,
+                errorMsg:
+                  "A message has been sent to you by email with instructions on how to reset your password."
+              });
+            }
+          } else {
+            // something goes worng
+            return res.json({
+              success: false,
+              errorMsg: "Something goes wrong please lets know"
+            });
+          }
+        } else {
+          return res.json({
+            success: false,
+            errorMsg: "Please confirme your account first"
+          });
+        }
+      } else {
+        return res.json({
+          success: false,
+          errorMsg:
+            "Invalid email or username. Please try to resubmit the form."
+        });
+      }
     }
+  } catch (error) {
+    return res.json({ success: false, errorMsg: "Error Occured" });
   }
 });
 
 // @route POST /api/users/passedit
-// @desc Recover USER
+// @desc Edit password
 // @access public
 router.post("/passedit", [middleware.register], async (req, res) => {
-  // first of all we have to make sure that we already have that token
-  const { token, password } = req.body;
-  const user = await userModel.findUserByRecovery(token);
-  if (user) {
-    let hash = bcrypt.hashSync(password, 10);
-    const update = await userModel.updatePassword(hash, user.id);
-    if (update) {
-      // now lets remove that token
-      const removeIt = await userModel.setRecovery(user.email, "");
-      if (removeIt)
-        res.json({
-          success: true,
-          errorMsg: "Your password has ben updated",
-          updated: "done",
-          valide: true
-        });
-      else
+  try {
+    // first of all we have to make sure that we already have that token
+    const { token, password } = req.body;
+    const user = await userModel.findUserByRecovery(token);
+    if (user) {
+      let hash = bcrypt.hashSync(password, 10);
+      const update = await userModel.updatePassword(hash, user.id);
+      if (update) {
+        // now lets remove that token
+        const removeIt = await userModel.setRecovery(user.email, "");
+        if (removeIt)
+          res.json({
+            success: true,
+            errorMsg: "Your password has ben updated",
+            updated: "done",
+            valide: true
+          });
+        else
+          res.json({
+            success: false,
+            errorMsg: "Problem With Removing The Token"
+          });
+      } else {
         res.json({
           success: false,
-          errorMsg: "Problem With Removing The Token"
+          errorMsg: "Something goes wrong please lets know"
         });
+      }
+    } else {
+      return res.json({
+        success: false,
+        errorMsg: "The reset token you have provided is not valid."
+      });
+    }
+  } catch (error) {
+    return res.json({ success: false, errorMsg: "Error Occured" });
+  }
+});
+
+// @route POST /api/users/checktoken
+// @desc checktoken
+// @access public
+router.get("/checktoken", async (req, res) => {
+  try {
+    let { token } = req.query;
+    // lets make sure its a valide token
+    const user = await userModel.findByToken(token);
+    if (user) {
+      res.json({ success: true, errorMsg: "YES", valide: true });
     } else {
       res.json({
         success: false,
-        errorMsg: "Something goes wrong please lets know"
+        errorMsg: "The reset token you have provided is not valid",
+        valide: false
       });
     }
-  } else {
-    return res.json({
-      success: false,
-      errorMsg: "The reset token you have provided is not valid."
-    });
-  }
-});
-router.get("/checktoken", async (req, res) => {
-  let { token } = req.query;
-  // lets make sure its a valide token
-  const user = await userModel.findByToken(token);
-  if (user) {
-    res.json({ success: true, errorMsg: "YES", valide: true });
-  } else {
-    res.json({
-      success: false,
-      errorMsg: "The reset token you have provided is not valid",
-      valide: false
-    });
+  } catch (error) {
+    return res.json({ success: false, errorMsg: "Error Occured" });
   }
 });
 
@@ -216,7 +241,7 @@ router.get("/current", [middleware.auth], async (req, res) => {
     delete user.password;
     res.json({ success: true, user });
   } catch (error) {
-    res.send("Server error");
+    return res.json({ success: false });
   }
 });
 
@@ -224,45 +249,49 @@ router.get("/current", [middleware.auth], async (req, res) => {
 // @desc    activation User
 // @access  Public
 router.get("/activation", async (req, res) => {
-  const { userName, token } = req.query;
-  let check = await userModel.checkActivation(userName);
-  if (check) {
-    // valide username
-    if (check.verified) {
-      //already verivied
-      return res.json({
-        success: false,
-        errorMsg: "Account Already Verified"
-      });
-    } else {
-      // lets work on validing this username
-      if (await userModel.ActivateUser(userName, token)) {
-        // the token is true
-        if (await userModel.updateValidation(userName, token)) {
-          // everything works fine
-          return res.json({
-            success: true,
-            errorMsg:
-              "Your account has been activated successfully. You can now login."
-          });
-        } else {
-          // something goes wrong
-          return res.json({
-            success: false,
-            errorMsg: "Something goes wrong please let us know"
-          });
-        }
-      } else {
-        //the token is not true
+  try {
+    const { userName, token } = req.query;
+    let check = await userModel.checkActivation(userName);
+    if (check) {
+      // valide username
+      if (check.verified) {
+        //already verivied
         return res.json({
           success: false,
-          errorMsg: "Sorry but this is invalide token you entre"
+          errorMsg: "Account Already Verified"
         });
+      } else {
+        // lets work on validing this username
+        if (await userModel.ActivateUser(userName, token)) {
+          // the token is true
+          if (await userModel.updateValidation(userName, token)) {
+            // everything works fine
+            return res.json({
+              success: true,
+              errorMsg:
+                "Your account has been activated successfully. You can now login."
+            });
+          } else {
+            // something goes wrong
+            return res.json({
+              success: false,
+              errorMsg: "Something goes wrong please let us know"
+            });
+          }
+        } else {
+          //the token is not true
+          return res.json({
+            success: false,
+            errorMsg: "Sorry but this is invalide token you entre"
+          });
+        }
       }
+    } else {
+      // invalide username
+      return res.json({ success: false, errorMsg: "invalid username" });
     }
-  } else {
-    // invalide username
-    return res.json({ success: false, errorMsg: "invalid username" });
+  } catch (error) {
+    return res.json({ success: false, errorMsg: "Error Occured" });
   }
 });
 
