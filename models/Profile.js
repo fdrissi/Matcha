d2720c7bf9aa4892a97b9165ec4cb80337db5c0f
Administrator <admin@example.com>
2019-12-09 15:54:26 +0100

    add trycatch block to profile model

--- models/Profile.js
+++ models/Profile.js
@@ -2,56 +2,72 @@ const { pool } = require("../config/db");
 const empty = require("is-empty");
 
 async function SetImage(id, photoname, counter) {
-  let sql;
-  switch (counter) {
-    case 0:
-      sql = `UPDATE photos SET first_Image = ?  WHERE id = ?`;
-      break;
-    case 1:
-      sql = `UPDATE photos SET second_Image = ?  WHERE id = ?`;
-      break;
-    case 2:
-      sql = `UPDATE photos SET third_Image = ?  WHERE id = ?`;
-      break;
-    case 3:
-      sql = `UPDATE photos SET fourth_Image = ?  WHERE id = ?`;
-      break;
-  }
-  const [result] = await pool.query(sql, [photoname, id]);
-  if (!empty(result)) {
-    return true;
-  } else {
+  try {
+    let sql;
+    switch (counter) {
+      case 0:
+        sql = `UPDATE photos SET first_Image = ?  WHERE id = ?`;
+        break;
+      case 1:
+        sql = `UPDATE photos SET second_Image = ?  WHERE id = ?`;
+        break;
+      case 2:
+        sql = `UPDATE photos SET third_Image = ?  WHERE id = ?`;
+        break;
+      case 3:
+        sql = `UPDATE photos SET fourth_Image = ?  WHERE id = ?`;
+        break;
+    }
+    const [result] = await pool.query(sql, [photoname, id]);
+    if (!empty(result)) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
     return false;
   }
 }
 
 async function setProfile(id, photoname) {
-  let sql = "UPDATE photos SET profile_Image = ? WHERE id = ?";
-  const [result] = await pool.query(sql, [photoname, id]);
-  if (!empty(result)) {
-    return true;
-  } else {
+  try {
+    let sql = "UPDATE photos SET profile_Image = ? WHERE id = ?";
+    const [result] = await pool.query(sql, [photoname, id]);
+    if (!empty(result)) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
     return false;
   }
 }
 
 async function setHolder(id) {
-  const Image_holder = "photo_holder.png";
-  let sql = `UPDATE photos SET profile_Image = ? WHERE id = ?`;
-  const [result] = await pool.query(sql, [Image_holder, id]);
-  if (!empty(result)) {
-    return true;
-  } else {
+  try {
+    const Image_holder = "photo_holder.png";
+    let sql = `UPDATE photos SET profile_Image = ? WHERE id = ?`;
+    const [result] = await pool.query(sql, [Image_holder, id]);
+    if (!empty(result)) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
     return false;
   }
 }
 
 async function getImage(id) {
-  let sql = `SELECT * FROM photos WHERE id = ?`;
-  const [result] = await pool.query(sql, [id]);
-  if (!empty(result)) {
-    return result[0];
-  } else {
+  try {
+    let sql = `SELECT * FROM photos WHERE id = ?`;
+    const [result] = await pool.query(sql, [id]);
+    if (!empty(result)) {
+      return result[0];
+    } else {
+      return false;
+    }
+  } catch (error) {
     return false;
   }
 }
@@ -65,195 +81,246 @@ async function imagesCounter(id, action) {
 }
 
 async function removeOnUpload(id, row) {
-  let sql = `SELECT ${row} FROM photos WHERE id = ?`;
-  const [result] = await pool.query(sql, [id]);
-  if (!empty(result)) {
-    return result[0][row];
-  } else {
+  try {
+    let sql = `SELECT ${row} FROM photos WHERE id = ?`;
+    const [result] = await pool.query(sql, [id]);
+    if (!empty(result)) {
+      return result[0][row];
+    } else {
+      return false;
+    }
+  } catch (error) {
     return false;
   }
 }
 
 async function getCounter(id) {
-  let sql = `SELECT counter FROM photos WHERE id = ?`;
-  const [result] = await pool.query(sql, [id]);
-  return result[0].counter;
+  try {
+    let sql = `SELECT counter FROM photos WHERE id = ?`;
+    const [result] = await pool.query(sql, [id]);
+    return result[0].counter;
+  } catch (error) {
+    return false;
+  }
 }
 
 async function setImageRow(id, row, filename) {
-  let sql = `UPDATE photos SET ${row} = ? WHERE id = ?`;
-  const [result] = await pool.query(sql, [filename, id]);
-  if (!empty(result)) {
-    return true;
-  } else {
+  try {
+    let sql = `UPDATE photos SET ${row} = ? WHERE id = ?`;
+    const [result] = await pool.query(sql, [filename, id]);
+    if (!empty(result)) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
     return false;
   }
 }
 
 async function setImageCover(id, filename) {
-  let sql = `UPDATE photos SET cover_Image = ? WHERE id = ?`;
-  const [result] = await pool.query(sql, [filename, id]);
-  if (!empty(result)) {
-    return true;
-  } else {
+  try {
+    let sql = `UPDATE photos SET cover_Image = ? WHERE id = ?`;
+    const [result] = await pool.query(sql, [filename, id]);
+    if (!empty(result)) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
     return false;
   }
 }
 
 async function getImageByRow(id, row) {
-  let sql = `SELECT ${row} FROM photos WHERE id = ?`;
-  const [result] = await pool.query(sql, [id]);
-  if (!empty(result)) {
-    return result[0][row];
-  } else {
+  try {
+    let sql = `SELECT ${row} FROM photos WHERE id = ?`;
+    const [result] = await pool.query(sql, [id]);
+    if (!empty(result)) {
+      return result[0][row];
+    } else {
+      return false;
+    }
+  } catch (error) {
     return false;
   }
 }
 
 async function fixPosition(id, row) {
-  const Image_holder = "photo_holder.png";
-  let counter;
-  let sql;
-  switch (row) {
-    case "first_Image":
-      counter = 0;
-      break;
-    case "second_Image":
-      counter = 1;
-      break;
-    case "third_Image":
-      counter = 2;
-      break;
-    default:
-      counter = 3;
-  }
-  while (counter < 4) {
-    switch (counter) {
-      case 0:
-        sql =
-          "UPDATE photos SET first_Image = second_Image, second_Image = ? WHERE id = ?";
+  try {
+    const Image_holder = "photo_holder.png";
+    let counter;
+    let sql;
+    switch (row) {
+      case "first_Image":
+        counter = 0;
         break;
-      case 1:
-        sql =
-          "UPDATE photos SET second_Image = third_Image, third_Image = ? WHERE id = ?";
+      case "second_Image":
+        counter = 1;
         break;
-      case 2:
-        sql =
-          "UPDATE photos SET third_Image = fourth_Image, fourth_Image = ? WHERE id = ?";
+      case "third_Image":
+        counter = 2;
         break;
-      case 3:
-        sql = "UPDATE photos SET fourth_Image = ? WHERE id = ?";
+      default:
+        counter = 3;
     }
-    const [result] = await pool.query(sql, [Image_holder, id]);
-    counter++;
+    while (counter < 4) {
+      switch (counter) {
+        case 0:
+          sql =
+            "UPDATE photos SET first_Image = second_Image, second_Image = ? WHERE id = ?";
+          break;
+        case 1:
+          sql =
+            "UPDATE photos SET second_Image = third_Image, third_Image = ? WHERE id = ?";
+          break;
+        case 2:
+          sql =
+            "UPDATE photos SET third_Image = fourth_Image, fourth_Image = ? WHERE id = ?";
+          break;
+        case 3:
+          sql = "UPDATE photos SET fourth_Image = ? WHERE id = ?";
+      }
+      const [result] = await pool.query(sql, [Image_holder, id]);
+      counter++;
+    }
+    return true;
+  } catch (error) {
+    return false;
   }
-  return true;
 }
 
 // user info
-
 async function getUserInfo(id) {
-  let sql =
-    "SELECT t2.first_name, t2.last_name, t1.* , DATE_FORMAT(t1.user_birth, '%Y-%m-%d') as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE t1.id = ?";
-  const [result] = await pool.query(sql, [id]);
-  return result[0];
+  try {
+    let sql =
+      "SELECT t2.first_name, t2.last_name, t1.* , DATE_FORMAT(t1.user_birth, '%Y-%m-%d') as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE t1.id = ?";
+    const [result] = await pool.query(sql, [id]);
+    return result[0];
+  } catch (error) {
+    return false;
+  }
 }
 
 // all user info
 async function getAllUserForBrowser(id, interesting, gender) {
-  let sql;
-  switch (interesting) {
-    case "Bisexual":
-      sql = `SELECT t2.first_name, t2.last_name, t1.* ,DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(t1.user_birth, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(t1.user_birth, '00-%m-%d'))  as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE t1.id != ? AND t2.verified = 1 AND t1.user_gender_interest = '${gender}' OR t1.id != '${id}' AND t2.verified = 1 AND  t1.user_gender_interest = "Bisexual"`;
-      break;
-    case "Male":
-      sql = `SELECT t2.first_name, t2.last_name, t1.* ,DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(t1.user_birth, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(t1.user_birth, '00-%m-%d'))  as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE t1.id != ? AND t1.user_gender = "Male" AND t2.verified = 1 AND t1.user_gender_interest = '${gender}' OR (t1.id != '${id}' AND t1.user_gender = "Male" AND t1.user_gender_interest = "Bisexual" AND t2.verified = 1)`;
-      break;
-    default:
-      sql = `SELECT t2.first_name, t2.last_name, t1.* ,DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(t1.user_birth, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(t1.user_birth, '00-%m-%d'))  as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE (t1.id != ? AND t1.user_gender = 'Female' AND t1.user_gender_interest = '${gender}' AND t2.verified = 1) OR (t1.id != '${id}' AND t1.user_gender = "Female" AND t1.user_gender_interest = "Bisexual" AND t2.verified = 1)`;
-      break;
+  try {
+    let sql;
+    switch (interesting) {
+      case "Bisexual":
+        sql = `SELECT t2.first_name, t2.last_name, t1.* ,DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(t1.user_birth, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(t1.user_birth, '00-%m-%d'))  as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE t1.id != ? AND t2.verified = 1 AND t1.user_gender_interest = '${gender}' OR t1.id != '${id}' AND t2.verified = 1 AND  t1.user_gender_interest = "Bisexual"`;
+        break;
+      case "Male":
+        sql = `SELECT t2.first_name, t2.last_name, t1.* ,DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(t1.user_birth, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(t1.user_birth, '00-%m-%d'))  as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE t1.id != ? AND t1.user_gender = "Male" AND t2.verified = 1 AND t1.user_gender_interest = '${gender}' OR (t1.id != '${id}' AND t1.user_gender = "Male" AND t1.user_gender_interest = "Bisexual" AND t2.verified = 1)`;
+        break;
+      default:
+        sql = `SELECT t2.first_name, t2.last_name, t1.* ,DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(t1.user_birth, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(t1.user_birth, '00-%m-%d'))  as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE (t1.id != ? AND t1.user_gender = 'Female' AND t1.user_gender_interest = '${gender}' AND t2.verified = 1) OR (t1.id != '${id}' AND t1.user_gender = "Female" AND t1.user_gender_interest = "Bisexual" AND t2.verified = 1)`;
+        break;
+    }
+    const [result] = await pool.query(sql, [id]);
+    return result;
+  } catch (error) {
+    return false;
   }
-  const [result] = await pool.query(sql, [id]);
-  return result;
 }
 
 //get browser users for by filter
 async function getFilterUserForBrowser(id, interesting, gender) {
-  let sql;
-  switch (interesting) {
-    case "Bisexual":
-      sql = `SELECT t2.first_name, t2.last_name, t1.* ,DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(t1.user_birth, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(t1.user_birth, '00-%m-%d'))  as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE t1.id != ? AND t2.verified = 1 AND t1.user_gender_interest = '${gender}' OR t1.id != '${id}' AND t2.verified = 1 AND  t1.user_gender_interest = "Bisexual"`;
-      break;
-    case "Male":
-      sql = `SELECT t2.first_name, t2.last_name, t1.* ,DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(t1.user_birth, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(t1.user_birth, '00-%m-%d'))  as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE t1.id != ? AND t1.user_gender = "Male" AND t2.verified = 1 AND t1.user_gender_interest = '${gender}' OR (t1.id != '${id}' AND t1.user_gender = "Male" AND t1.user_gender_interest = "Bisexual" AND t2.verified = 1)`;
-      break;
-    default:
-      sql = `SELECT t2.first_name, t2.last_name, t1.* ,DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(t1.user_birth, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(t1.user_birth, '00-%m-%d'))  as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE (t1.id != ? AND t1.user_gender = 'Female' AND t1.user_gender_interest = '${gender}' AND t2.verified = 1) OR (t1.id != '${id}' AND t1.user_gender = "Female" AND t1.user_gender_interest = "Bisexual" AND t2.verified = 1)`;
-      break;
+  try {
+    let sql;
+    switch (interesting) {
+      case "Bisexual":
+        sql = `SELECT t2.first_name, t2.last_name, t1.* ,DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(t1.user_birth, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(t1.user_birth, '00-%m-%d'))  as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE t1.id != ? AND t2.verified = 1 AND t1.user_gender_interest = '${gender}' OR t1.id != '${id}' AND t2.verified = 1 AND  t1.user_gender_interest = "Bisexual"`;
+        break;
+      case "Male":
+        sql = `SELECT t2.first_name, t2.last_name, t1.* ,DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(t1.user_birth, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(t1.user_birth, '00-%m-%d'))  as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE t1.id != ? AND t1.user_gender = "Male" AND t2.verified = 1 AND t1.user_gender_interest = '${gender}' OR (t1.id != '${id}' AND t1.user_gender = "Male" AND t1.user_gender_interest = "Bisexual" AND t2.verified = 1)`;
+        break;
+      default:
+        sql = `SELECT t2.first_name, t2.last_name, t1.* ,DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(t1.user_birth, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(t1.user_birth, '00-%m-%d'))  as user_birth FROM user_info t1 INNER JOIN users t2 ON t1.id = t2.id WHERE (t1.id != ? AND t1.user_gender = 'Female' AND t1.user_gender_interest = '${gender}' AND t2.verified = 1) OR (t1.id != '${id}' AND t1.user_gender = "Female" AND t1.user_gender_interest = "Bisexual" AND t2.verified = 1)`;
+        break;
+    }
+    const [result] = await pool.query(sql, [id]);
+    return result;
+  } catch (error) {
+    return false;
   }
-  const [result] = await pool.query(sql, [id]);
-  return result;
 }
 
 // get user info by row
 async function getUserInfoByRow(id, row) {
-  let sql = `SELECT ${row} FROM user_info WHERE id = ?`;
-  const [result] = await pool.query(sql, [id, id]);
-  return result[0][row];
+  try {
+    let sql = `SELECT ${row} FROM user_info WHERE id = ?`;
+    const [result] = await pool.query(sql, [id, id]);
+    return result[0][row];
+  } catch (error) {
+    return false;
+  }
 }
 
 async function updateUserInfo(data, id) {
-  const {
-    user_gender,
-    user_gender_interest,
-    user_relationship,
-    user_tags,
-    user_birth_day,
-    user_birth_year,
-    user_birth_month,
-    user_current_occupancy,
-    user_city,
-    user_biography,
-    user_location
-  } = data;
-  const set_from_map = user_location.lat && user_location.lng ? true : false;
-  const tags = JSON.stringify(user_tags);
-  if (user_birth_day && user_birth_month && user_birth_day)
-    user_bith = `${user_birth_year}-${user_birth_month}-${user_birth_day}`;
-  else user_bith = null;
-  let sql =
-    "update user_info SET user_gender = ?  ,user_gender_interest = ? ,user_relationship = ? , user_tags = ? , user_birth = ?, user_city = ?, user_lat = ?, user_lng = ? , user_current_occupancy = ?, user_biography = ? , set_from_map = ?   WHERE id = ?";
-  const [result] = await pool.query(sql, [
-    user_gender,
-    user_gender_interest,
-    user_relationship,
-    tags,
-    user_bith,
-    user_city,
-    user_location.lat,
-    user_location.lng,
-    user_current_occupancy,
-    user_biography,
-    set_from_map,
-    id
-  ]);
-  if (result.changedRows) {
-    return true;
-  } else {
+  try {
+    const {
+      user_gender,
+      user_gender_interest,
+      user_relationship,
+      user_tags,
+      user_birth_day,
+      user_birth_year,
+      user_birth_month,
+      user_current_occupancy,
+      user_city,
+      user_biography,
+      user_location
+    } = data;
+    const set_from_map = user_location.lat && user_location.lng ? true : false;
+    const tags = JSON.stringify(user_tags);
+    if (user_birth_day && user_birth_month && user_birth_day)
+      user_bith = `${user_birth_year}-${user_birth_month}-${user_birth_day}`;
+    else user_bith = null;
+    let sql =
+      "update user_info SET user_gender = ?  ,user_gender_interest = ? ,user_relationship = ? , user_tags = ? , user_birth = ?, user_city = ?, user_lat = ?, user_lng = ? , user_current_occupancy = ?, user_biography = ? , set_from_map = ?   WHERE id = ?";
+    const [result] = await pool.query(sql, [
+      user_gender,
+      user_gender_interest,
+      user_relationship,
+      tags,
+      user_bith,
+      user_city,
+      user_location.lat,
+      user_location.lng,
+      user_current_occupancy,
+      user_biography,
+      set_from_map,
+      id
+    ]);
+    if (result.changedRows) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
     return false;
   }
 }
 
 async function getResultByRow(row, id) {
-  let sql = `SELECT ${row} from user_info WHERE id = ?`;
-  const [result] = await pool.query(sql, [id]);
-  return result[0][row];
+  try {
+    let sql = `SELECT ${row} from user_info WHERE id = ?`;
+    const [result] = await pool.query(sql, [id]);
+    return result[0][row];
+  } catch (error) {
+    return false;
+  }
 }
 
 async function updateGeoLocation(latitude, longitude, id) {
-  let sql = "UPDATE user_info SET user_lat = ?, user_lng = ? WHERE id = ?";
-  await pool.query(sql, [latitude, longitude, id]);
-  return true;
+  try {
+    let sql = "UPDATE user_info SET user_lat = ?, user_lng = ? WHERE id = ?";
+    await pool.query(sql, [latitude, longitude, id]);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 async function likeProfile(userId, profileId) {
