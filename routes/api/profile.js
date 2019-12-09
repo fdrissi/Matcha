dcd6c4c38877dcc17a15bc4d8976da4b0e426223
Administrator <admin@example.com>
2019-12-09 16:19:00 +0100

    add trycatch block to profile api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -46,48 +46,58 @@ router.post(
   "/upload/:row",
   [middleware.auth, upload.single("myImage")],
   async (req, res) => {
-    const file = req.file;
-    const { row } = req.params;
-    const id = req.user.id;
-    const image = new Jimp(file.path, async function(err, image) {
-      if (!err && file) {
-        let filename = id + "/" + file.filename;
-        const remove = await profileModel.removeOnUpload(id, row);
-        const counter = await profileModel.getCounter(id);
-        if (row === "profile_Image")
-          check = await profileModel.setProfile(id, filename);
-        else if (remove !== "photo_holder.png" && row !== "profile_Image")
-          check = await profileModel.setImageRow(id, row, filename);
-        else check = await profileModel.SetImage(id, filename, counter);
-        const result = await profileModel.getImage(id);
-        const cover = await profileModel.getImageByRow(id, "cover_Image");
-        if (check) {
-          if (remove !== "photo_holder.png" && remove !== `${id}/profile.png`) {
-            await unlinkAsync(`./client/public/uploads/${remove}`);
-            if (remove === cover)
-              await profileModel.setImageCover(id, "cover_holder.png");
+    try {
+      const file = req.file;
+      const { row } = req.params;
+      const id = req.user.id;
+      const image = new Jimp(file.path, async function(err, image) {
+        if (!err && file) {
+          let filename = id + "/" + file.filename;
+          const remove = await profileModel.removeOnUpload(id, row);
+          const counter = await profileModel.getCounter(id);
+          if (row === "profile_Image")
+            check = await profileModel.setProfile(id, filename);
+          else if (remove !== "photo_holder.png" && row !== "profile_Image")
+            check = await profileModel.setImageRow(id, row, filename);
+          else check = await profileModel.SetImage(id, filename, counter);
+          const result = await profileModel.getImage(id);
+          const cover = await profileModel.getImageByRow(id, "cover_Image");
+          if (check) {
+            if (
+              remove !== "photo_holder.png" &&
+              remove !== `${id}/profile.png`
+            ) {
+              await unlinkAsync(`./client/public/uploads/${remove}`);
+              if (remove === cover)
+                await profileModel.setImageCover(id, "cover_holder.png");
+            }
+            if (remove === "photo_holder.png" && row !== "profile_Image")
+              await profileModel.imagesCounter(id, "add");
+            return res.json({
+              success: true,
+              errorMsg: "Your image hass been uploaded 🤘",
+              result
+            });
+          } else {
+            return res.json({
+              success: false,
+              errorMsg: "There is an error on upload image api 😱"
+            });
           }
-          if (remove === "photo_holder.png" && row !== "profile_Image")
-            await profileModel.imagesCounter(id, "add");
-          return res.json({
-            success: true,
-            errorMsg: "Your image hass been uploaded 🤘",
-            result
-          });
         } else {
+          await unlinkAsync(file.path);
           return res.json({
             success: false,
-            errorMsg: "There is an error on upload image api 😱"
+            errorMsg: "Please upload a Valide image 😠"
           });
         }
-      } else {
-        await unlinkAsync(file.path);
-        return res.json({
-          success: false,
-          errorMsg: "Please upload a Valide image 😠"
-        });
-      }
-    });
+      });
+    } catch (error) {
+      return res.json({
+        success: false,
+        errorMsg: "Error Occured"
+      });
+    }
   }
 );
 
@@ -95,22 +105,29 @@ router.post(
 // @desc    Get user images
 // @access  Private
 router.get("/getImage", [middleware.auth], async (req, res) => {
-  const id =
-    req.query.id && (await userModel.findById(req.query.id))
-      ? req.query.id
-      : req.user.id;
-  const result = await profileModel.getImage(id);
-  if (result) {
-    delete result.id;
-    delete result.counter;
-    result.loading = false;
-    // for (var k in result) payload[k] = result[k];
+  try {
+    const id =
+      req.query.id && (await userModel.findById(req.query.id))
+        ? req.query.id
+        : req.user.id;
+    const result = await profileModel.getImage(id);
+    if (result) {
+      delete result.id;
+      delete result.counter;
+      result.loading = false;
+      // for (var k in result) payload[k] = result[k];
+      return res.json({
+        success: true,
+        result
+      });
+    }
     return res.json({
-      success: true,
-      result
+      success: false
     });
-  } else {
-    console.log(error);
+  } catch (error) {
+    return res.json({
+      success: false
+    });
   }
 });
 
@@ -165,7 +182,10 @@ router.post("/setCover", [middleware.auth], async (req, res) => {
       );
     }
   } catch (error) {
-    console.log(error);
+    return res.json({
+      success: false,
+      errorMsg: "Error Occured"
+    });
   }
 });
 
@@ -173,44 +193,52 @@ router.post("/setCover", [middleware.auth], async (req, res) => {
 // @desc    Remove user images
 // @access  Private
 router.delete("/removeImage", [middleware.auth], async (req, res) => {
-  const { filed, photo } = req.body;
-  const id = req.user.id;
-  switch (photo) {
-    case "photo_holder.png":
-      return res.json({
-        success: false,
-        errorMsg: "You Can't Delete This image 😤"
-      });
-    default:
-      const check = await profileModel.getImageByRow(id, filed);
-      if (check !== photo) {
+  try {
+    const { filed, photo } = req.body;
+    const id = req.user.id;
+    switch (photo) {
+      case "photo_holder.png":
+        return res.json({
+          success: false,
+          errorMsg: "You Can't Delete This image 😤"
+        });
+      default:
+        const check = await profileModel.getImageByRow(id, filed);
+        if (check !== photo) {
+          const result = await profileModel.getImage(id);
+          return res.json({
+            success: false,
+            errorMsg: "You Can't Delete This photo",
+            result
+          });
+        } else if (filed === "profile_Image") await profileModel.setHolder(id);
+        else {
+          await profileModel.fixPosition(id, filed);
+        }
         const result = await profileModel.getImage(id);
-        return res.json({
-          success: false,
-          errorMsg: "You Can't Delete This photo",
-          result
-        });
-      } else if (filed === "profile_Image") await profileModel.setHolder(id);
-      else {
-        await profileModel.fixPosition(id, filed);
-      }
-      const result = await profileModel.getImage(id);
-      if (result) {
-        if (filed !== "profile_Image") profileModel.imagesCounter(id, "delete");
-        await unlinkAsync(`./client/public/uploads/${photo}`);
-        await profileModel.setImageCover(id, "cover_holder.png");
-        return res.json({
-          success: true,
-          errorMsg: "Your image hass been Deleted 🗑️",
-          result
-        });
-      } else {
-        return res.json({
-          success: false,
-          errorMsg: "Something Goes Wrong",
-          result
-        });
-      }
+        if (result) {
+          if (filed !== "profile_Image")
+            profileModel.imagesCounter(id, "delete");
+          await unlinkAsync(`./client/public/uploads/${photo}`);
+          await profileModel.setImageCover(id, "cover_holder.png");
+          return res.json({
+            success: true,
+            errorMsg: "Your image hass been Deleted 🗑️",
+            result
+          });
+        } else {
+          return res.json({
+            success: false,
+            errorMsg: "Something Goes Wrong",
+            result
+          });
+        }
+    }
+  } catch (error) {
+    return res.json({
+      success: false,
+      errorMsg: "Error occured"
+    });
   }
 });
 
@@ -218,44 +246,49 @@ router.delete("/removeImage", [middleware.auth], async (req, res) => {
 // @route   Post api/profle/getBrowser
 // @desc    get Browser data
 // @access  Private
-
 router.get("/getBrowser/", [middleware.auth], async (req, res) => {
-  const id = req.user.id;
-  console.log(id);
-  const interesting = await profileModel.getUserInfoByRow(
-    id,
-    "user_gender_interest"
-  );
-  const lat = await profileModel.getUserInfoByRow(id, "user_lat");
-  const long = await profileModel.getUserInfoByRow(id, "user_lng");
-  const gender = await profileModel.getUserInfoByRow(id, "user_gender");
-  const data = await profileModel.getAllUserForBrowser(id, interesting, gender);
-  function distance(lat1, lon1, lat2, lon2) {
-    var p = 0.017453292519943295; // Math.PI / 180
-    var c = Math.cos;
-    var a =
-      0.5 -
-      c((lat2 - lat1) * p) / 2 +
-      (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
-    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
-  }
-  data.map(value => {
-    const destination = parseFloat(
-      distance(lat, long, value.user_lat, value.user_lng).toFixed(2)
+  try {
+    const id = req.user.id;
+    const interesting = await profileModel.getUserInfoByRow(
+      id,
+      "user_gender_interest"
     );
-    value.destination = destination;
-  });
-  data.sort((a, b) =>
-    a.destination > b.destination ? 1 : b.destination > a.destination ? -1 : 0
-  );
-  return res.json(data);
+    const lat = await profileModel.getUserInfoByRow(id, "user_lat");
+    const long = await profileModel.getUserInfoByRow(id, "user_lng");
+    const gender = await profileModel.getUserInfoByRow(id, "user_gender");
+    const data = await profileModel.getAllUserForBrowser(
+      id,
+      interesting,
+      gender
+    );
+    function distance(lat1, lon1, lat2, lon2) {
+      var p = 0.017453292519943295; // Math.PI / 180
+      var c = Math.cos;
+      var a =
+        0.5 -
+        c((lat2 - lat1) * p) / 2 +
+        (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
+      return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+    }
+    data.map(value => {
+      const destination = parseFloat(
+        distance(lat, long, value.user_lat, value.user_lng).toFixed(2)
+      );
+      value.destination = destination;
+    });
+    data.sort((a, b) =>
+      a.destination > b.destination ? 1 : b.destination > a.destination ? -1 : 0
+    );
+    return res.json(data);
+  } catch (error) {
+    return false;
+  }
 });
 
 //  Workibng on the browser
 // @route   Post api/profle/getFilter
 // @desc    filter for the browser
 // @access  Private
-
 router.get(
   "/getFilter",
   [middleware.auth, middleware.browse_filter],
@@ -320,59 +353,65 @@ router.get(
 // @desc    get user info
 // @access  Private
 router.get("/getUserInfo/", [middleware.auth], async (req, res) => {
-  const id =
-    req.query.id && (await userModel.findById(+req.query.id))
-      ? +req.query.id
-      : req.user.id;
-  id !== req.user.id &&
-    (await profileModel.setNotification(id, req.user.id, "visit"));
-  const result = await profileModel.getUserInfo(+id);
-  const liked = await profileModel.isUserLikedProfile(+req.user.id, +id);
-  const blocked = await profileModel.isUserBlockedProfile(+req.user.id, id);
-  const matched = await profileModel.areMatched(+req.user.id, +id);
-  var obj = JSON.parse(result.user_tags);
-  const [year, month, day] = result.user_birth
-    ? result.user_birth.split("-")
-    : "";
+  try {
+    const id =
+      req.query.id && (await userModel.findById(+req.query.id))
+        ? +req.query.id
+        : req.user.id;
+    id !== req.user.id &&
+      (await profileModel.setNotification(id, req.user.id, "visit"));
+    const result = await profileModel.getUserInfo(+id);
+    const liked = await profileModel.isUserLikedProfile(+req.user.id, +id);
+    const blocked = await profileModel.isUserBlockedProfile(+req.user.id, id);
+    const matched = await profileModel.areMatched(+req.user.id, +id);
+    var obj = JSON.parse(result.user_tags);
+    const [year, month, day] = result.user_birth
+      ? result.user_birth.split("-")
+      : "";
 
-  const age = result.user_birth
-    ? moment().diff(`${year}-${month}-${day}`, "years")
-    : null;
-  const fameRate = await profileModel.getFameRate(id);
-  const my_info = {
-    loading: false,
-    liked,
-    matched,
-    blocked,
-    id,
-    user_first_name: result.first_name,
-    user_last_name: result.last_name,
-    user_gender: result.user_gender,
-    user_relationship: result.user_relationship,
-    user_birth_day: day,
-    user_tags: obj ? obj : [],
-    user_birth_month: month,
-    user_current_occupancy: result.user_current_occupancy,
-    user_gender_interest: result.user_gender_interest,
-    user_birth_year: year,
-    user_age: age,
-    user_city: result.user_city,
-    user_biography: result.user_biography,
-    user_location: {
-      lat: parseFloat(result.user_lat, 10),
-      lng: parseFloat(result.user_lng, 10)
-    },
-    user_fame_rate: fameRate,
-    user_set_from_map: result.set_from_map
-  };
-  let get_info = JSON.stringify(my_info, function(key, value) {
-    return value === undefined ? "" : value;
-  });
-  let info = JSON.parse(get_info);
-  return res.json({
-    success: true,
-    info
-  });
+    const age = result.user_birth
+      ? moment().diff(`${year}-${month}-${day}`, "years")
+      : null;
+    const fameRate = await profileModel.getFameRate(id);
+    const my_info = {
+      loading: false,
+      liked,
+      matched,
+      blocked,
+      id,
+      user_first_name: result.first_name,
+      user_last_name: result.last_name,
+      user_gender: result.user_gender,
+      user_relationship: result.user_relationship,
+      user_birth_day: day,
+      user_tags: obj ? obj : [],
+      user_birth_month: month,
+      user_current_occupancy: result.user_current_occupancy,
+      user_gender_interest: result.user_gender_interest,
+      user_birth_year: year,
+      user_age: age,
+      user_city: result.user_city,
+      user_biography: result.user_biography,
+      user_location: {
+        lat: parseFloat(result.user_lat, 10),
+        lng: parseFloat(result.user_lng, 10)
+      },
+      user_fame_rate: fameRate,
+      user_set_from_map: result.set_from_map
+    };
+    let get_info = JSON.stringify(my_info, function(key, value) {
+      return value === undefined ? "" : value;
+    });
+    let info = JSON.parse(get_info);
+    return res.json({
+      success: true,
+      info
+    });
+  } catch (error) {
+    return res.json({
+      success: true
+    });
+  }
 });
 
 // @route   Post api/profle/updateSettingInfo
@@ -424,7 +463,10 @@ router.post(
         });
       }
     } catch (error) {
-      console.log(error);
+      return res.json({
+        success: false,
+        errorMsg: "Error Occured"
+      });
     }
   }
 );
@@ -433,44 +475,47 @@ router.post(
 // @desc    set userLocation Value
 // @access  private
 router.post("/setUserLocation", [middleware.auth], async (req, res) => {
-  const id = req.user.id;
-  const { latitude, longitude, error } = req.body.data;
-  const result = await profileModel.getResultByRow("set_from_map", id);
-  if (error) {
-    if (error && !result) {
-      const ipAddress = await publicIp.v4();
-      iplocation(ipAddress, [], async (error, resp) => {
-        if (!error) {
-          if (
-            await profileModel.updateGeoLocation(
-              resp.latitude,
-              resp.longitude,
-              id
+  try {
+    const id = req.user.id;
+    const { latitude, longitude, error } = req.body.data;
+    const result = await profileModel.getResultByRow("set_from_map", id);
+    if (error) {
+      if (error && !result) {
+        const ipAddress = await publicIp.v4();
+        iplocation(ipAddress, [], async (error, resp) => {
+          if (!error) {
+            if (
+              await profileModel.updateGeoLocation(
+                resp.latitude,
+                resp.longitude,
+                id
+              )
             )
-          )
-            return res.status(200).send("user didn't accepte set location");
-        } else {
-          res.status(200).send("error in getting ip address");
-        }
-      });
-    } else {
-      return res.status(200).send("set_from_map TRUE");
-    }
-  } else {
-    if (!result) {
-      if (await profileModel.updateGeoLocation(latitude, longitude, id)) {
-        return res.status(200).send("USER ACCEPTE");
+              return res.status(200).send("user didn't accepte set location");
+          } else {
+            res.status(200).send("error in getting ip address");
+          }
+        });
+      } else {
+        return res.status(200).send("set_from_map TRUE");
       }
     } else {
-      return res.status(200).send("set_from_map TRUE ");
+      if (!result) {
+        if (await profileModel.updateGeoLocation(latitude, longitude, id)) {
+          return res.status(200).send("USER ACCEPTE");
+        }
+      } else {
+        return res.status(200).send("set_from_map TRUE ");
+      }
     }
+  } catch (error) {
+    return false;
   }
 });
 
 // @route   Get api/profle/getpreedefined
 // @desc    get preedefined data
 // @access  public
-
 router.get("/getpreedefined", async (req, res) => {
   try {
     res.json({
@@ -478,7 +523,9 @@ router.get("/getpreedefined", async (req, res) => {
       predefined
     });
   } catch (error) {
-    console.log(error);
+    res.json({
+      success: false
+    });
   }
 });
 
@@ -503,7 +550,9 @@ router.post("/userLikeProfile", middleware.auth, async (req, res) => {
       success: result
     });
   } catch (error) {
-    console.log(error);
+    return res.json({
+      success: false
+    });
   }
 });
 
@@ -525,7 +574,9 @@ router.post("/isUserLikedProfile", middleware.auth, async (req, res) => {
       success: result
     });
   } catch (error) {
-    console.log(error);
+    return res.json({
+      success: false
+    });
   }
 });
 
@@ -546,7 +597,9 @@ router.post("/areMatched", middleware.auth, async (req, res) => {
       success: result
     });
   } catch (error) {
-    console.log(error);
+    return res.json({
+      success: false
+    });
   }
 });
 
@@ -570,7 +623,9 @@ router.post("/userBlockProfile", middleware.auth, async (req, res) => {
       success: result
     });
   } catch (error) {
-    console.log(error);
+    return res.json({
+      success: false
+    });
   }
 });
 
@@ -592,7 +647,9 @@ router.post("/isUserBlockedProfile", middleware.auth, async (req, res) => {
       success: result
     });
   } catch (error) {
-    console.log(error);
+    return res.json({
+      success: false
+    });
   }
 });
 
@@ -614,7 +671,9 @@ router.post("/reportProfile", middleware.auth, async (req, res) => {
       success: result
     });
   } catch (error) {
-    console.log(error);
+    return res.json({
+      success: false
+    });
   }
 });
 
