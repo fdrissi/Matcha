5ba17d1385bcddc6012584c97e954938be191a3a
Administrator <admin@example.com>
2019-12-23 20:37:30 +0100

    adding global upload path

---
+++ client/build/precache-manifest.cdbe491bf76fd4cd32e1d7bcf85098c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca97c05ee1ad5dc61d58b6ef5fb406ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4764db1b9e7cc98f55a3",
+    "url": "/static/js/2.2e4a3f26.chunk.js"
+  },
+  {
+    "revision": "034bbdc270ae6d32828b2097549823a0",
+    "url": "/static/js/2.2e4a3f26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aeb819c16515a7ec159f",
+    "url": "/static/js/main.b7b959c1.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
