5ad563e3a0906cec13a77bccd9a12641c59f1293
Administrator <admin@example.com>
2019-12-25 00:40:55 +0100

    delete builtin suggestions

--- client/src/Components/profile/Edit_Profile.js
+++ client/src/Components/profile/Edit_Profile.js
@@ -53,43 +53,6 @@ let _ = require("lodash");
 
 /* autocomplete */
 
-const suggestions = [
-  "Afghanistan",
-  "Aland Islands",
-  "Albania",
-  "Algeria",
-  "American Samoa",
-  "Andorra",
-  "Angola",
-  "Anguilla",
-  "Antarctica",
-  "Antigua and Barbuda",
-  "Argentina",
-  "Armenia",
-  "Aruba",
-  "Australia",
-  "Austria",
-  "Azerbaijan",
-  "Bahamas",
-  "Bahrain",
-  "Bangladesh",
-  "Barbados",
-  "Belarus",
-  "Belgium",
-  "Belize",
-  "Benin",
-  "Bermuda",
-  "Bhutan",
-  "Bolivia, Plurinational State of",
-  "Bonaire, Sint Eustatius and Saba",
-  "Bosnia and Herzegovina",
-  "Botswana",
-  "Bouvet Island",
-  "Brazil",
-  "British Indian Ocean Territory",
-  "Brunei Darussalam"
-];
-
 function renderInput(inputProps) {
   const { value, onChange, chips, ref, ...other } = inputProps;
 
